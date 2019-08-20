35badf5cc28ba2a30c3ddc5ec85606e126b56b72
Chris Johnson <chrisltd@gmail.com>
2019-08-20 09:18:37 -0400

    Added functionality for SSB

--- Discord SSB/ViewController.swift
+++ Discord SSB/ViewController.swift
@@ -7,21 +7,60 @@
 //
 
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate {
 
+    var webView: WKWebView!
+    
+    let webUrl = "https://discordapp.com/channels/@me"
+    let baseDomain = "discordapp.com"
+    let webName = "Discord"
+    
+    override func loadView() {
+        super.loadView()
+        
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let myURL = URL(string:webUrl)
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+    
+        self.view.window?.title = webName
+    }
+    
+    // Open external links in Safari
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        if navigationAction.navigationType != .linkActivated {
+            // not a click
+            decisionHandler(.allow)
+            return
         }
+        
+        let url = navigationAction.request.url
+        
+        if url?.description.lowercased().range(of: baseDomain) != nil {
+            // open in app
+            decisionHandler(.allow)
+        } else {
+            // open in Safari
+            decisionHandler(.cancel)
+            NSWorkspace.shared.open(url!)
+        }
+        
     }
-
-
+    
 }
-
