9513bfa547b3a8219e0480ac69bb51256741cbfe
Chris Johnson <chrisltd@gmail.com>
2019-08-20 09:03:50 -0400

    Initial Commit

---
+++ Discord SSB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Discord SSB
+//
+//  Created by Chris Johnson on 8/20/19.
+//  Copyright © 2019 Yoeyo, Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
